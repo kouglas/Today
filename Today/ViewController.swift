cd711027737995a84f28f00c1e685b4c08cc4af7
Kari <kouglas2@gmail.com>
2024-11-29 16:42:57 -0600

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Kari Douglas on 11/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodayTests/TodayTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodayTests.swift
+//  TodayTests
+//
+//  Created by Kari Douglas on 11/29/24.
+//
+
+import Testing
+@testable import Today
+
+struct TodayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
